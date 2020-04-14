6b84e41d313a55061f5e47482a785914fed422b1
Damian <damian.kursa.web@gmail.com>
2020-04-14 21:50:46 +0200

    Create product working

--- ecom-front/src/admin/AddProduct.js
+++ ecom-front/src/admin/AddProduct.js
@@ -1,53 +1,167 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import Layout from "../layouts/Main"
-import {isAuthenticated} from '../auth/index'
-import {createProduct} from './apiAdmin'
+import React, { useState, useEffect } from "react";
+import Layout from "../layouts/Main";
+import { isAuthenticated } from "../auth/index";
+import { Link } from "react-router-dom";
+import { createProduct } from "../admin/apiAdmin";
 
+const AddProduct = () => {
 
-const AddProduct = () =>{
-  const { user , token } = isAuthenticated()
-  const { values, setValues} = useState({
-    name:'',
-    description:'',
-    price:'',
-    category:'',
-    shipping:'',
-    quantity:'',
-    photo:'',
-    loading:false,
-    error:'',
-    createProduct:'',
-    redirectToProfile:false,
-    formData:'',
-  })
-  const {
-    name,
-    description,
-    price,
-    category,
-    shipping,
-    quantity,
-    photo,
-    loading,
-    error,
-    createProduct,
-    redirectToProfile,
-    formData,
-  } = values
-  return (
-    <Layout
-      title="Add a new product"
-      description={`${user.name}, are you ready to create new product?`}
-      className="container col-md-8 offset-md-2"
-    >
-      <div className="row">
-        <div className="col-md-8 offset-md-2">
+    const [values, setValues] = useState({
+        name: "",
+        description: "",
+        price: "",
+        categories: [],
+        category: "",
+        shipping: "",
+        quantity: "",
+        photo: "",
+        loading: false,
+        error: "",
+        createdProduct: "",
+        redirectToProfile: false,
+        formData: ""
+    });
 
-        </div>
-      </div>
-    </Layout>
-  )
-}
+    const {
+        name,
+        description,
+        price,
+        categories,
+        category,
+        shipping,
+        quantity,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData
+    } = values;
 
-export default AddProduct
+    const { user, token } = isAuthenticated();
+
+    useEffect(() => {
+        setValues({ ...values, formData: new FormData() });
+    }, []);
+
+    const handleChange = name => event => {
+        const value =
+            name === "photo" ? event.target.files[0] : event.target.value;
+        formData.set(name, value);
+        setValues({ ...values, [name]: value });
+    };
+
+    const clickSubmit = event => {
+        event.preventDefault();
+        setValues({...values, error:'', loading: true});
+        createProduct(user._id, token, formData)
+        .then(data =>{
+          if(data.error){
+            setValues({...values, error: data.error})
+          }else{
+            setValues({
+              ...values,
+              name:'',
+              description:'',
+              photo:'',
+              price:'',
+              quantity:'',
+              loading:false,
+              createdProduct: data.name
+            })
+          }
+        })
+    };
+
+    const newPostForm = () => (
+        <form className="mb-3" onSubmit={clickSubmit}>
+            <h4>Post Photo</h4>
+            <div className="form-group">
+                <label className="btn btn-secondary">
+                    <input
+                        onChange={handleChange("photo")}
+                        type="file"
+                        name="photo"
+                        accept="image/*"
+                    />
+                </label>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input
+                    onChange={handleChange("name")}
+                    type="text"
+                    className="form-control"
+                    value={name}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Description</label>
+                <textarea
+                    onChange={handleChange("description")}
+                    className="form-control"
+                    value={description}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Price</label>
+                <input
+                    onChange={handleChange("price")}
+                    type="number"
+                    className="form-control"
+                    value={price}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Category</label>
+                <select
+                    onChange={handleChange("category")}
+                    className="form-control"
+                >
+                    <option value="5e86470c81286f0f66f29172">First category</option>
+                    <option value="5e8876849453222e54b8b123">Second Category</option>
+
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Shipping</label>
+                <select
+                    onChange={handleChange("shipping")}
+                    className="form-control"
+                >
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Quantity</label>
+                <input
+                    onChange={handleChange("quantity")}
+                    type="number"
+                    className="form-control"
+                    value={quantity}
+                />
+            </div>
+
+            <button className="btn btn-outline-primary">Create Product</button>
+        </form>
+    );
+
+    return (
+        <Layout
+            title="Add a new product"
+            description={`G'day ${user.name}, ready to add a new product?`}
+        >
+            <div className="row">
+                <div className="col-md-8 offset-md-2">{newPostForm()}</div>
+            </div>
+        </Layout>
+    );
+};
+
+export default AddProduct;
